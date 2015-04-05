1bb2f8bdc6343f17c4237e61234e6619e16decbc
zhouqiangWang <zhouq.wang@gmail.com>
2015-04-05 23:41:27 +0800

    test for Native SSH
    test for Native SSH

--- circularImageView/src/main/java/com/mikhaellopez/circularimageview/CircularImageView.java
+++ circularImageView/src/main/java/com/mikhaellopez/circularimageview/CircularImageView.java
@@ -80,15 +80,18 @@ public class CircularImageView extends ImageView {
 			if(canvas.getHeight()<canvasSize)
 				canvasSize = canvas.getHeight();
 
-			BitmapShader shader = new BitmapShader(Bitmap.createScaledBitmap(image, canvasSize, canvasSize, false), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+			BitmapShader shader = new BitmapShader(Bitmap.createScaledBitmap(
+                    image, canvasSize, canvasSize, false), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
 			paint.setShader(shader);
 
 			// circleCenter is the x or y of the view's center
 			// radius is the radius in pixels of the cirle to be drawn
 			// paint contains the shader that will texture the shape
 			int circleCenter = (canvasSize - (borderWidth * 2)) / 2;
-			canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth, ((canvasSize - (borderWidth * 2)) / 2) + borderWidth - 4.0f, paintBorder);
-			canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth, ((canvasSize - (borderWidth * 2)) / 2) - 4.0f, paint);
+			canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth,
+                    ((canvasSize - (borderWidth * 2)) / 2) + borderWidth - 4.0f, paintBorder);
+			canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth,
+                    ((canvasSize - (borderWidth * 2)) / 2) - 4.0f, paint);
 		}
 	}
 
